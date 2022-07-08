7cdb8a8f2d3082bb42a177c5fc34a04157b67fa7
uen0_ <sarasa4869@zoho.com>
2022-07-08 12:45:05 +0900

    [Update]　課題2 周囲の地雷の数を数える

--- Assets/Minesweeper.cs
+++ Assets/Minesweeper.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,6 +20,8 @@ public class Minesweeper : MonoBehaviour
 
     [SerializeField]
     private Cell _cellPrefab = null;
+
+    List<Cell> cells = new List<Cell>();
 
     void Start()
     {
@@ -48,14 +53,24 @@ public class Minesweeper : MonoBehaviour
         {
             Mine(_cells);
         }
+
+        for (var r = 0; r < _rows; r++)
+        {
+            for (var c = 0; c < _columns; c++)
+            {
+                Check(_cells, r, c);
+            }
+        }
     }
+
+
 
     /// <summary>地雷設置処理</summary>
     /// <param name="_cells"></param>
     private void Mine(Cell[,] cells)
     {
-        var r = Random.Range(0, _rows);
-        var c = Random.Range(0, _columns);
+        var r = UnityEngine.Random.Range(0, _rows);
+        var c = UnityEngine.Random.Range(0, _columns);
         var cell = cells[r, c];
         if (cell.GetComponent<Cell>().CellState != CellState.Mine)
         {
@@ -66,9 +81,112 @@ public class Minesweeper : MonoBehaviour
             Mine(cells);
         }
     }
-
-    void MineCheck()
+    /// <summary>
+    /// 地雷の個数判定
+    /// </summary>
+    /// <param name="_cells"></param>
+    /// <param name="r"></param>
+    /// <param name="c"></param>
+    private void Check(Cell[,] _cells, int r, int c)
     {
-        
+        int _mineNum = 0;
+
+        //自身が地雷かの判定
+        if (_cells[r, c].CellState != CellState.Mine)
+        {
+            //左上
+            if (r != 0 && c != 0 && _cells[r - 1, c - 1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //上
+            if (r != 0 && _cells[r-1, c].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //右上
+            if (r != 0 && c != _columns - 1 && _cells[r - 1, c + 1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //左
+            if (c != 0 && _cells[r, c-1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //右
+            if (c !=_columns-1 && _cells[r , c+1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //左下
+            if (c != 0 && r != _rows-1 && _cells[r + 1, c - 1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //下
+            if (r != _rows - 1 && _cells[r+1, c].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+            //右下
+            if (c != _columns-1 && r != _rows - 1 && _cells[r + 1, c + 1].CellState == CellState.Mine)
+            {
+                _mineNum++;
+            }
+        }
+
+        if (_cells[r, c].CellState != CellState.Mine)
+        {
+            ChangeState(_cells, r, c, _mineNum);
+        }
     }
+    /// <summary>
+    /// 地雷の数に応じてセルのStateを変える
+    /// </summary>
+    /// <param name="_cells"></param>
+    /// <param name="r"></param>
+    /// <param name="c"></param>
+    /// <param name="_mineNum"></param>
+    private static void ChangeState(Cell[,] _cells, int r, int c, int _mineNum)
+    {
+        if (_mineNum == 0)
+        {
+            _cells[r, c].CellState = CellState.None;
+        }
+        if (_mineNum == 1)
+        {
+            _cells[r, c].CellState = CellState.One;
+        }
+        if (_mineNum == 2)
+        {
+            _cells[r, c].CellState = CellState.Two;
+        }
+        if (_mineNum == 3)
+        {
+            _cells[r, c].CellState = CellState.Three;
+        }
+        if (_mineNum == 4)
+        {
+            _cells[r, c].CellState = CellState.Four;
+        }
+        if (_mineNum == 5)
+        {
+            _cells[r, c].CellState = CellState.Five;
+        }
+        if (_mineNum == 6)
+        {
+            _cells[r, c].CellState = CellState.Six;
+        }
+        if (_mineNum == 7)
+        {
+            _cells[r, c].CellState = CellState.Seven;
+        }
+        if (_mineNum == 8)
+        {
+            _cells[r, c].CellState = CellState.Eight;
+        }
+    }
+
+
 }
